5e63c9636391646a79e282098e7aad84e1d04624
fiodar miculich <feotheodor@gmail.com>
2020-10-31 16:45:45 +0300

    feat: add task;

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,22 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  constructor(direct){
-    this.direct = !direct;
-  }
 
+	constructor(Direct) {
+		this.Direct = (Direct === false);
+	}
 
-  encrypt(message, key) {
-    if(!message || !key) return new Error();
+	encrypt(str, key) {
+		if (!str || !key) throw new Error;
+		key = key.repeat(Math.ceil(str.length / key.length));
+		let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		let result = "";
+		let keyIndex = 0;
+		for (let i = 0; i < str.length; i++) {
+			if (/^[a-zA-Z]{1}$/.test(str[i])) {
+				result += alphabet.charAt((alphabet.indexOf(str.charAt(i)) + alphabet.indexOf(key.charAt(i - keyIndex))) % alphabet.length);
+			}
+			else {
+				result += str[i];
+				keyIndex++;
+			};
 
-  }    
-  decrypt(message,key) {
-    if(!message || !key){
-      return new Error();
-    }else{
-
-    }
-  }
+		}
+		result = this.Direct ? [...result].reverse().join('') : result;
+		return result.toUpperCase();
+	}
+	decrypt(str, key) {
+		if (!str || !key) throw new Error;
+		key = key.repeat(Math.ceil(str.length / key.length));
+		let keyIndex = 0;
+		let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		let result = "";
+		for (let i = 0; i < str.length; i++) {
+			if (/^[a-zA-Z]{1}$/.test(str[i])) {
+				result += alphabet.charAt(((alphabet.indexOf(str.charAt(i)) + alphabet.length) - alphabet.indexOf(key.charAt(i - keyIndex))) % alphabet.length);
+			}
+			else {
+				result += str[i];
+				keyIndex++;
+			};
+		}
+		result = this.Direct ? [...result].reverse().join('') : result;
+		return result.toUpperCase();
+	}
 }
 
 module.exports = VigenereCipheringMachine;
